4bb9aac43db10a9bcd713ea86d10188e18050445
Cody Bloemhard <codybloemhard@gmail.com>
2025-09-20 17:53:04 +0200

    added: emphasis;

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # incodoc-to-ansi
 
-Convert incodoc to ANSI.
+Convert incodoc to ANSI: plain text with with formatting and colours ready to display in a terminal.
+
+Corporeal elements to be configurable.
+
+- terminal width
+- colour and effect
+- margin
+- border
+- newlines
+- states that change formatting (eg. clicked link, highlighted paragraph, colapsed)
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,13 @@
 use incodoc::*;
 
-#[derive(Clone, Copy, Default, Hash, Debug, Eq, PartialEq, Ord, PartialOrd)]
+use zen_colour::*;
+
+use std::fmt::Write;
+
+#[derive(Clone, Default, Hash, Debug, Eq, PartialEq, Ord, PartialOrd)]
 pub struct Context {
     pub ps: ParStatus,
+    pub modifier: String,
 }
 
 #[derive(Clone, Copy, Default, Hash, Debug, Eq, PartialEq, Ord, PartialOrd)]
@@ -19,9 +24,14 @@ pub enum ParStatus {
     Element,
 }
 
+/// Take an incodoc and unparse it to ANSI.
+/// Use just this function unless doing something fancy.
 pub fn doc_to_ansi_string(doc: &Doc) -> String {
     let mut res = String::new();
-    let mut context = Context::default();
+    let mut context = Context {
+        modifier: RESET.to_string(),
+        ..Default::default()
+    };
     doc_to_ansi(doc, &mut context, &mut res);
     res
 }
@@ -43,18 +53,36 @@ pub fn paragraph_to_ansi(par: &Paragraph, c: &mut Context, output: &mut String) 
     for item in &par.items {
         match item {
             ParagraphItem::Text(text) => {
-                print!("{}", format_text(text, c));
+                text_to_ansi(text, c, output);
             },
             ParagraphItem::MText(TextWithMeta { text, tags, props }) => {
-                print!("{}", format_text(text, c));
+                text_to_ansi(text, c, output);
             },
-            ParagraphItem::Em(emphasis) => {},
+            ParagraphItem::Em(emphasis) => {
+                emphasis_to_ansi(emphasis, c, output);
+            },
             ParagraphItem::Link(link) => {},
             ParagraphItem::Code(code) => {},
             ParagraphItem::List(list) => {},
             ParagraphItem::Table(table) => {},
         }
     }
+}
+
+pub fn emphasis_to_ansi(em: &Emphasis, c: &mut Context, output: &mut String) {
+    let modifier = match (em.etype, em.strength) {
+        (EmType::Emphasis, EmStrength::Light) => ITALIC.to_string(),
+        (EmType::Emphasis, EmStrength::Medium) => BOLD.to_string(),
+        (EmType::Emphasis, EmStrength::Strong) => format!("{ITALIC}{BOLD}"),
+        (EmType::Deemphasis, EmStrength::Light) => FAINT.to_string(),
+        (EmType::Deemphasis, EmStrength::Medium) => CROSSED.to_string(),
+        (EmType::Deemphasis, EmStrength::Strong) => HIDDEN.to_string(),
+    };
+    let _ = write!(output, "{modifier}{}{}", format_text(&em.text, c), c.modifier);
+}
+
+pub fn text_to_ansi(text: &str, c: &mut Context, output: &mut String) {
+    let _ = write!(output, "{}", format_text(text, c));
 }
 
 pub fn format_text(text: &str, c: &mut Context) -> String {
